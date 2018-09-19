13cafc4d5e3fb89d242524ae67051ae53ed8d532
TAKE72K <hyes941073@gmail.com>
2018-09-20 00:27:45 +0800

    kk

--- anka.py
+++ anka.py
@@ -84,7 +84,7 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("newanka", new_anka))
     #recieve callback data
-    db.add_handler(CallbackQueryHandler(callback_re))
+    dp.add_handler(CallbackQueryHandler(callback_re))
     # log all errors
     dp.add_error_handler(error)
 
